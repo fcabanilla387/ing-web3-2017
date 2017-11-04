626cbf39236c57c445bc837c02a2892c41d92494
root <root@DESKTOP-ENH7VO9.localdomain>
2017-11-04 20:33:57 -0300

    se termino el servicio calcularDuracionTotal()

--- Modulo/src/test/java/ar/com/magm/model/ListaCancionesTest.java
+++ Modulo/src/test/java/ar/com/magm/model/ListaCancionesTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import ar.com.magm.ti.model.service.impl.ListaCancionesService;
 import ar.com.magm.ti.service.exception.ServiceException;
 import java.util.ArrayList;
+import org.junit.Assert;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -25,9 +26,14 @@ public class ListaCancionesTest extends BaseTest {
         ArrayList<Cancion> canciones = new ArrayList();
         Cancion c1 = new Cancion();
         Cancion c2 = new Cancion();
+        Cancion c3 = new Cancion();
         c1.setDuracion("74'1''");
         c2.setDuracion("1'20''");
+        c3.setDuracion("30240'0''");
+        canciones.add(c1);
+        canciones.add(c2);
+        canciones.add(c3);
         String str = service.getDuracionTotalLista(canciones);
-        assertNotNull("Se generó la duracion total", str);
+        Assert.assertEquals("Se genero la duracion total de la lista.", "3w0d25h15m21s", str);
     }
 }
